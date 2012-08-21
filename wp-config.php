307846b0578425b306b7ac6a0a8a3c25fc906fdb
Ryan Brainard <brainard@heroku.com>
2012-08-21 15:00:54 -0700

    read db from env

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+
+define('DB_URL', $_ENV['CLEARDB_DATABASE_URL']);
+
+define('DB_NAME', basename(parse_url(DB_URL, PHP_URL_PATH)));
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', parse_url(DB_URL, PHP_URL_USER));
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', parse_url(DB_URL, PHP_URL_PASS));
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', parse_url(DB_URL, PHP_URL_HOST));
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
